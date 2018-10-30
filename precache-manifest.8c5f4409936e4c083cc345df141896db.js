8e37649053f66990d8a040c253435034be8c1327
George Tsendra <georgetsendraua@ukr.net>
2018-10-30 18:39:11 +0200

    Updates

---
+++ precache-manifest.8c5f4409936e4c083cc345df141896db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebb94c2a26ab7e14785c",
+    "url": "/Kanban_Board/static/js/runtime~main.ebb94c2a.js"
+  },
+  {
+    "revision": "c33cd17c67ca84f17772",
+    "url": "/Kanban_Board/static/js/main.c33cd17c.chunk.js"
+  },
+  {
+    "revision": "fb900dcfa57e2ec48a8d",
+    "url": "/Kanban_Board/static/js/1.fb900dcf.chunk.js"
+  },
+  {
+    "revision": "c33cd17c67ca84f17772",
+    "url": "/Kanban_Board/static/css/main.55dc9ca2.chunk.css"
+  },
+  {
+    "revision": "032015195fd3e389abfbe7bad6431df4",
+    "url": "/Kanban_Board/index.html"
+  }
+];
